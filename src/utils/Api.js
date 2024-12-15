9f07bca826c1f806645953d0cd0b7e235e1e2171
Prashant Jain <prashantjain0002@gmail.com>
2024-12-15 13:19:56 +0530

    Update ENV File

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const BASE_URL = "https://api.themoviedb.org/3";
-const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
+// const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
+const TMDB_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMjBhMWZiMzVkOTQ4NjAzZWZmMDMxMjgxYWFlZDc4MCIsInN1YiI6IjY2MDUyNjYwMjgxMWExMDE4NmRhYjdkNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-v-nKvX81KjDX8_HPlyowZ2f9Uv2SKGE8fiChotS8iw";
 
 const headers = {
   Authorization: "bearer " + TMDB_TOKEN,
